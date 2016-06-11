05eb10295d4a1cd608818399e47682126cf7c54b
bhserna <bhserna@gmail.com>
2016-06-11 18:25:57 -0500

    rendering with sinatra

---
+++ 01_inventory/app.rb
@@ -0,0 +1,16 @@
+require "sinatra"
+require_relative "inventory"
+
+class Store
+  def all_articles
+    [{name: "Camisa 1", code: "c1", quantity: 10},
+     {name: "Gorra 1", code: "g1", quantity: 35}]
+  end
+end
+
+get '/' do
+  store = Store.new
+  inventory = Inventory.new(store)
+  @articles = inventory.articles_list
+  erb :index
+end
