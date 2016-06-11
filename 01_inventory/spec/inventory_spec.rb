14230ccab85b325359b9b21b604cb4a41a56bf3c
bhserna <bhserna@gmail.com>
2016-06-11 17:46:39 -0500

    first test

--- 01_inventory/spec/inventory_spec.rb
+++ 01_inventory/spec/inventory_spec.rb
@@ -1,9 +1,24 @@
 require "rspec"
 
 RSpec.describe "Inventory" do
+  class FakeStore
+    def initialize(records)
+    end
+  end
+
   describe "shows the articles" do
-    it "with name"
+    it "with name" do
+      store = store_with([{name: "Camisa 1"}, {name: "Gorra 1"}])
+      inventory = Inventory.new(store)
+      articles = inventory.articles_list
+      expect(articles.map(&:name)).to eq ["Camisa 1", "Gorra 1"]
+    end
+
     it "with code"
     it "with quantity"
+
+    def store_with(records)
+      FakeStore.new(records)
+    end
   end
 end
