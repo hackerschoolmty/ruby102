19f7cfcad107a7bbfc043ca09695d796061c6a82
bhserna <bhserna@gmail.com>
2016-06-11 19:13:10 -0500

    adds create method to Store

--- 01_inventory/app.rb
+++ 01_inventory/app.rb
@@ -1,14 +1,30 @@
 require "sinatra"
 require_relative "inventory"
 
+INITIAL_RECORDS = [
+  {"name" => "Camisa 1", "code" => "c1", "quantity" => 10},
+  {"name" => "Gorra 1", "code" => "g1", "quantity" => 35}
+]
+
 class Store
-  def all_articles
-    [{name: "Camisa 1", code: "c1", quantity: 10},
-     {name: "Gorra 1", code: "g1", quantity: 35}]
+  def initialize(records)
+    @records = records
   end
+
+  def create(record)
+    records << record
+  end
+
+  def all_articles
+    records
+  end
+
+  private
+
+  attr_reader :records
 end
 
-store = Store.new
+store = Store.new(INITIAL_RECORDS)
 inventory = Inventory.new(store)
 
 get '/' do
