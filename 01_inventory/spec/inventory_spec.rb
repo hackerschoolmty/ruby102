0495b8fd724925ab594a214d7fefd85e37a01911
bhserna <bhserna@gmail.com>
2016-06-14 19:29:15 -0500

    our first test to show inventory articles

--- 01_inventory/spec/inventory_spec.rb
+++ 01_inventory/spec/inventory_spec.rb
@@ -1,9 +1,33 @@
 require "rspec"
 
+class Inventory
+  def initialize(store)
+  end
+
+  def articles_list
+    []
+  end
+end
+
 RSpec.describe "Inventory" do
+  class FakeStore
+    def initialize(records)
+    end
+  end
+
   describe "shows the articles" do
-    it "with name"
+    it "with name" do
+      store = store_with([{name: "Camisa 1"}, {name: "Gorra 1"}])
+      inventory = Inventory.new(store)
+      articles = inventory.articles_list
+      expect(articles.map(&:name)).to eq ["Camisa 1", "Gorra 1"]
+    end
+
     it "with code"
     it "with quantity"
+
+    def store_with(records)
+      FakeStore.new(records)
+    end
   end
 end
